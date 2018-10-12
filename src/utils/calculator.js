53380eaa5045fe267d6da7fa791de63d32aebd42
Pete Hawkins <pete@alternatelabs.co>
2018-10-12 20:35:19 +0100

    Get most operations running

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -1,9 +1,38 @@
+const isOperation = str => ["divide", "multiply", "subtract", "add"].indexOf(str) > -1;
+const hasLeftNumber = state => state.left.length;
+const hasRightNumber = state => state.right.length;
+const hasOperation = state => !!state.operation;
+const hasPendingOperation = state => (
+  hasLeftNumber(state) &&
+  hasRightNumber(state) &&
+  hasOperation(state)
+);
+const evaluate = state => {
+  const left = parseFloat(state.left, 10);
+  const right = parseFloat(state.right, 10);
+  switch (state.operation) {
+    case "divide":
+      return left / right;
+    case "multiply":
+      return left * right;
+    case "subtract":
+      return left - right;
+    case "add":
+      return left + right;
+  }
+};
+
 export default (state, input) => {
   let output = {
     left: state.left || "",
-    right: state.right || null,
+    right: state.right || "",
     operation: state.operation || null,
   };
+
+  if (hasPendingOperation(output) && isOperation(input)) {
+    output.left = String(evaluate(output));
+    output.right = "";
+  }
 
   switch (input) {
     case "clear":
@@ -13,18 +42,31 @@ export default (state, input) => {
     case "percent":
       break;
     case "divide":
-      break;
     case "multiply":
-      break;
     case "subtract":
-      break;
     case "add":
+      if (hasLeftNumber(output)) {
+        output.operation = input;
+      }
       break;
     case "equals":
+      if (hasPendingOperation(output)) {
+        output.left = String(evaluate(output));
+        output.right = "";
+        output.operation = null;
+      }
       break;
     default:
-      if (input !== 0 || input.length) {
-        output.left += input;
+      if (state.operation) {
+        // Working on the RHS
+        if ((input !== "." && input !== 0) || hasRightNumber(output)) {
+          output.right += input;
+        }
+      } else {
+        // Working on the LHS
+        if ((input !== "." && input !== 0) || hasLeftNumber(output)) {
+          output.left += input;
+        }
       }
       break;
   }
--- src/utils/calculator.test.js
+++ src/utils/calculator.test.js
@@ -7,7 +7,7 @@ describe("Calculator", () => {
 
       expect(ouput).toEqual({
         left: "7",
-        right: null,
+        right: "",
         operation: null
       });
     });
@@ -17,11 +17,127 @@ describe("Calculator", () => {
 
       expect(ouput).toEqual({
         left: "",
-        right: null,
+        right: "",
         operation: null
       });
     });
-    it("doesn't allow . to input", () => {});
-    it("doesn't operations", () => {});
+
+    it("doesn't allow . to input", () => {
+      const ouput = calculator({}, ".")
+
+      expect(ouput).toEqual({
+        left: "",
+        right: "",
+        operation: null
+      });
+    });
+
+    it("doesn't allow operations", () => {
+      const ouput = calculator({}, "divide")
+
+      expect(ouput).toEqual({
+        left: "",
+        right: "",
+        operation: null
+      });
+    });
+  });
+
+  describe("when left is provided", () => {
+    it("concatenates number entry", () => {
+      const ouput = calculator({ left: "13" }, 0)
+
+      expect(ouput).toEqual({
+        left: "130",
+        right: "",
+        operation: null
+      });
+    });
+
+    it("allows . to input", () => {
+      const ouput = calculator({ left: "7" }, ".")
+
+      expect(ouput).toEqual({
+        left: "7.",
+        right: "",
+        operation: null
+      });
+    });
+
+    it("doesn't allow operations", () => {
+      const ouput = calculator({ left: "65" }, "divide")
+
+      expect(ouput).toEqual({
+        left: "65",
+        right: "",
+        operation: "divide"
+      });
+    });
+  });
+
+  describe("when left and operation are provided", () => {
+    it("edits right number", () => {
+      const ouput = calculator({ left: "13", operation: "add" }, 2)
+
+      expect(ouput).toEqual({
+        left: "13",
+        right: "2",
+        operation: "add"
+      });
+    });
+
+    it("changes operation", () => {
+      const ouput = calculator({ left: "65", operation: "add" }, "divide")
+
+      expect(ouput).toEqual({
+        left: "65",
+        right: "",
+        operation: "divide"
+      });
+    });
+  });
+
+  describe("when full input is provided", () => {
+    it("can be calculated", () => {
+      const ouput = calculator({ left: "13", right: "12", operation: "add" }, "equals")
+
+      expect(ouput).toEqual({
+        left: "25",
+        right: "",
+        operation: null
+      });
+    });
+
+    it("operation evaluates and then sets new operation", () => {
+      const ouput = calculator({ left: "2", right: "2", operation: "add" }, "divide")
+
+      expect(ouput).toEqual({
+        left: "4",
+        right: "",
+        operation: "divide"
+      });
+    });
+  });
+
+  describe("basic operations", () => {
+    it("can add", () => {
+      const ouput = calculator({ left: "13", right: "12", operation: "add" }, "equals")
+
+      expect(ouput).toEqual({
+        left: "25",
+        right: "",
+        operation: null
+      });
+    });
+
+    it("can subtract", () => {
+      const ouput = calculator({ left: "13", right: "14", operation: "subtract" }, "equals")
+
+      expect(ouput).toEqual({
+        left: "-1",
+        right: "",
+        operation: null
+      });
+    });
   });
 });
