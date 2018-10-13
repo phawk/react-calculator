e25c06a5ccab5ca2696fb5fdb2be51db5a74758e
Pete Hawkins <pete@alternatelabs.co>
2018-10-13 11:36:54 +0100

    Disable elastic scrolling on iOS

--- src/index.js
+++ src/index.js
@@ -10,8 +10,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// Disable elastic scrolling on iOS
-document.ontouchmove = function (e) {
-  e.preventDefault();
-};
