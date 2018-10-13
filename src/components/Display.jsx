39affebd09f3144a2e08f0d002d9977f31ea7574
Pete Hawkins <pete@alternatelabs.co>
2018-10-13 11:58:49 +0100

    Scale font size based on length

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Screen = styled.div`
-  color: #fff;
   padding: 10px 24px;
-  font-size: 44px;
+  color: #fff;
   font-weight: 300;
   text-align: right;
+  font-size: ${props => props.fontSize}px;
+  line-height: 48px;
 `;
 
-export default (props) => (
-  <Screen>
-    { props.right ? props.right : props.left }
-  </Screen>
-);
+export default (props) => {
+  const text = props.right ? props.right : props.left;
+  let fontSize = 44;
+  if (text.length > 10) {
+    fontSize = 44 - ((text.length - 10) * 2);
+  }
+  if (fontSize < 14) {
+    fontSize = 14;
+  }
+  return (
+    <Screen fontSize={fontSize}>
+      { text }
+    </Screen>
+  );
+}
